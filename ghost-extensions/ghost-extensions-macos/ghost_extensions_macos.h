f6aedbfeb7eac78126bfe5526bea2aa1c8f7f8ff
Ben Roth <spongebucket@gmail.com>
2018-08-08 10:53:38 -0700

    cocoa: split extensions into separate ghost-extensions framework

---
+++ ghost-extensions/ghost-extensions-macos/ghost_extensions_macos.h
@@ -0,0 +1,18 @@
+//
+//  ghost_extensions_macos.h
+//  ghost-extensions-macos
+//
+//  Created by Ben Roth on 8/8/18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ghost_extensions_macos.
+FOUNDATION_EXPORT double ghost_extensions_macosVersionNumber;
+
+//! Project version string for ghost_extensions_macos.
+FOUNDATION_EXPORT const unsigned char ghost_extensions_macosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ghost_extensions_macos/PublicHeader.h>
+
+
---
+++ ghost-extensions/ghost-extensions/ghost_extensions.h
@@ -0,0 +1,18 @@
+//
+//  ghost_extensions.h
+//  ghost-extensions
+//
+//  Created by Ben Roth on 8/8/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ghost_extensions.
+FOUNDATION_EXPORT double ghost_extensionsVersionNumber;
+
+//! Project version string for ghost_extensions.
+FOUNDATION_EXPORT const unsigned char ghost_extensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ghost_extensions/PublicHeader.h>
+
+
