2a9b0e0ed328fafc8e7738ae1841346356babbb4
Jesse Ruder <jesse@getexponent.com>
2018-09-12 12:04:30 -0700

    plist changes

--- love/src/cefsimple_mac.mm
+++ love/src/cefsimple_mac.mm
@@ -120,7 +120,15 @@ int main(int argc, char* argv[]) {
 
   // Specify CEF global settings here.
   CefSettings settings;
-
+	settings.log_severity = LOGSEVERITY_VERBOSE;
+	
+	NSString* resultTemp = @"/Users/jesseruder/Downloads/cef_binary_3.3440.1806.g65046b7_macosx64/Release";
+	NSData* data = [resultTemp dataUsingEncoding:NSUTF16StringEncoding];
+	NSString* result = [[NSString alloc] initWithData:data encoding:NSUTF16StringEncoding];
+	
+  /*cef_string_set((char16 *) data.bytes, result.length,
+				   &settings.framework_dir_path, true);
+*/
   // SimpleApp implements application-level callbacks for the browser process.
   // It will create the first browser instance in OnContextInitialized() after
   // CEF has initialized.
