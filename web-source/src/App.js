5678649aab273ad5964ef6c37149cc4cd9f13d45
Jim Lee <send@jimmy.email>
2018-09-12 18:27:55 -0700

    [ghost] goes with a simpler approach to copy over UI logic from the web repo

---
+++ web-source/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>HELLO CHARLIE, NIKKI, JESSE, BEN AND JASON I AM HERE NOW</div>;
+
+export default App;
---
+++ web-source/webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  devServer: {
+    port: 3000,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+};
