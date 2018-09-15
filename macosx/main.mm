94fd9991e3df2eb8eac508e849d9a054ab5f15d3
Ben Roth <spongebucket@gmail.com>
2018-09-14 18:32:28 -0700

    macos: this file is small enough to be main.mm now

--- macosx/main.mm
+++ macosx/main.mm
@@ -1,12 +1,4 @@
-// Copyright (c) 2013 The Chromium Embedded Framework Authors.
-// Portions copyright (c) 2010 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 #import <Cocoa/Cocoa.h>
-
-#include <SDL.h>
-#include <SDL_syswm.h>
 
 #import "GhostAppDelegate.h"
 
