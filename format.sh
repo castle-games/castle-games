1b838dd4992133d0f5b7c18a63707ecf4b5317f0
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-09-18 13:06:34 -0700

    fix 'format.sh' hitting non-existent files

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #prettier --write js/*.js *.js
-clang-format -i -style=file macosx/*.{h,m*,c*} macosx/ghost-macosx_Helper/*.cpp
+clang-format -i -style=file macosx/*.{h,m*} macosx/ghost-macosx_Helper/*.cpp
