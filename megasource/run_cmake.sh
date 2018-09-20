22118322d5fe2b6eb636b8eb7297e9911931fe4e
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-09-20 16:42:35 -0700

    force "/MD" plz

--- megasource/run_cmake.sh
+++ megasource/run_cmake.sh
@@ -1,4 +1,5 @@
 cmake -DMEGA_LOVE=../love \
 	-DOPENSSL_ROOT_DIR=../openssl/openssl-1.0.2l-vs2017 -DOPENSSL_USE_STATIC_LIBS=TRUE -DOPENSSL_MSVC_STATIC_RT=TRUE \
+	-DCEF_RUNTIME_LIBRARY_FLAG='//MD' \
 	-G "Visual Studio 15" -H. -Bbuild
 # cmake --build build --target love/love --config Release
