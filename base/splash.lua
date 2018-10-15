d09e9116038a6676f6509cab46a6c60e85f5d7e1
Ben Roth <spongebucket@gmail.com>
2018-10-15 15:57:12 -0700

    base: clean up ghost loading screen
    closes https://github.com/expo/ghost/issues/71

--- base/splash.lua
+++ base/splash.lua
@@ -1,34 +1,29 @@
 splash = {
-   _timer = 2,
-   _index = 3,
-   _MAX_INDEX = 16,
+   _timer = 1,
+   _value = 0,
+   _valueGoal = 1,
 }
 
 function splash:draw()
    local width, height = love.window.getMode()
    love.graphics.push('all')
    love.graphics.translate(width * 0.5, height * 0.5)
-   love.graphics.scale(1.5)
-   local x, y = -width * 0.2, -256
-   for index = 1, self._MAX_INDEX do
-      if index == self._index then
-         love.graphics.setColor(1, 1, 1, 1)
-      else
-         love.graphics.setColor(1, 1, 1, 0.5)
-      end
-      love.graphics.print('Loading...', x, y)
-      y = y + 32
-   end
+   love.graphics.scale(2.0)
+   local x, y = -width * 0.22, -height * 0.22
+   love.graphics.setColor(1, 1, 1, self._value)
+   love.graphics.print('Loading...', x, y)
    love.graphics.pop()
 end
 
 function splash:update(dt)
+   self._value = self._value + (self._valueGoal - self._value) * 0.15
    self._timer = self._timer - dt
    if self._timer <= 0 then
-      self._timer = 0.2 -- start animating if it's taking too long.
-      self._index = self._index + 1
-      if self._index > self._MAX_INDEX then
-         self._index = 1
+      self._valueGoal = 1.0 - self._valueGoal
+      if self._valueGoal == 0 then
+         self._timer = 0.4
+      else
+         self._timer = 1
       end
    end
 end
