fee384fc7b228d1dc09645cfb20daa7210ea5207
Jesse Ruder <jesse@getexponent.com>
2018-12-13 11:42:54 -0800

    Revert "lua: fix splash.lua on server"
    This reverts commit 81d30ce7ea0a706e199b6ab88152e007b6c1eae5.

--- base/splash.lua
+++ base/splash.lua
@@ -4,47 +4,38 @@ splash = {
    _shineVelocity = 800,
    _textY = 0,
    _textYTimer = 0,
-   _imgMaskRooks = nil,
-   _imgMaskText = nil,
-   _imgSize = nil,
 }
 
-function splash:loadImg()
-   if self._imgMaskRooks == nil then
-      self._imgMaskRooks = love.graphics.newImage('assets/loading-mask-rooks.png')
-      self._imgMaskText = love.graphics.newImage('assets/loading-mask-text.png')
-      self._imgSize = { width = 500, height = 250 }
-   end
-end
+local imgMaskRooks = love.graphics.newImage('assets/loading-mask-rooks.png')
+local imgMaskText = love.graphics.newImage('assets/loading-mask-text.png')
+local imgSize = { width = 500, height = 250 }
 
 function splash:draw()
-   self:loadImg()
-
    local width, height = love.window.getMode()
    love.graphics.push('all')
    love.graphics.translate(width * 0.5, height * 0.5)
    love.graphics.scale(0.5, 0.5)
-   love.graphics.translate(-self._imgSize.width * 0.5, -self._imgSize.height * 0.5)
+   love.graphics.translate(-imgSize.width * 0.5, -imgSize.height * 0.5)
    love.graphics.setColor(1, 1, 1, 0.7)
-   love.graphics.rectangle('fill', 0, 0, self._imgSize.width, self._imgSize.height)
-   if self._shineX > 18 and self._shineX < self._imgSize.width - 18 then
+   love.graphics.rectangle('fill', 0, 0, imgSize.width, imgSize.height)
+   if self._shineX > 18 and self._shineX < imgSize.width - 18 then
       love.graphics.push('all')
       love.graphics.setBlendMode('add')
       love.graphics.setColor(1, 1, 1, 0.3)
-      love.graphics.rectangle('fill', self._shineX - 18, 0, 36, self._imgSize.height)
-      love.graphics.rectangle('fill', self._shineX - 6, 0, 12, self._imgSize.height)
+      love.graphics.rectangle('fill', self._shineX - 18, 0, 36, imgSize.height)
+      love.graphics.rectangle('fill', self._shineX - 6, 0, 12, imgSize.height)
       love.graphics.pop()
    end
    love.graphics.setColor(1, 1, 1, 1)
    love.graphics.draw(
-      self._imgMaskText,
+      imgMaskText,
       0, 4 + self._textY,
       0,
       1, 1,
       0, 0
    )
    love.graphics.draw(
-      self._imgMaskRooks,
+      imgMaskRooks,
       0, 0,
       0,
       1, 1,
@@ -54,8 +45,6 @@ function splash:draw()
 end
 
 function splash:update(dt)
-   self:loadImg()
-
    self._shineTimer = self._shineTimer - dt
    if self._shineTimer <= 0 then
       self._shineX = 0
@@ -63,7 +52,7 @@ function splash:update(dt)
       self._shineTimer = 1.75
    end
    
-   if self._shineX < self._imgSize.width then
+   if self._shineX < imgSize.width then
       self._shineX = self._shineX + self._shineVelocity * dt
       self._shineVelocity = self._shineVelocity - 100 * dt
    end
