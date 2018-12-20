26b631d902de84a8c56de2bbb53de77dc9291822
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-12-19 17:19:23 -0800

    simplify `GhostView`

--- mobile/js/GhostView.js
+++ mobile/js/GhostView.js
@@ -3,18 +3,4 @@
 import React from 'react';
 import { requireNativeComponent } from 'react-native';
 
-export default class GhostView extends React.Component {
-  static Native = requireNativeComponent('GhostView', null);
-
-  state = {
-    reloadCounter: 0,
-  };
-
-  render() {
-    return <GhostView.Native key={this.state.reloadCounter} {...this.props} />;
-  }
-
-  reload() {
-    this.setState(({ reloadCounter }) => ({ reloadCounter: reloadCounter + 1 }));
-  }
-}
+export default requireNativeComponent('GhostView', null);
