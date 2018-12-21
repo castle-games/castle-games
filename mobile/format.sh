60d389d3152aca4c123581a7f492ae4e64206f0e
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-12-20 20:29:25 -0800

    remove scripts I no longer use

--- mobile/format.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-prettier --write js/*.js *.js
-clang-format -i -style=file ios/castle/*.{h,m*}
--- mobile/ios/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# deploy
-ios-deploy --debug --bundle /tmp/castle-build/castle.app
