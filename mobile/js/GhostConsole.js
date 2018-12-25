71eb6e4989af00f3b09116637d4e23b20823836c
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-12-24 19:15:03 -0800

    mobile: log lua error stacktraces

--- mobile/js/GhostConsole.js
+++ mobile/js/GhostConsole.js
@@ -2,4 +2,7 @@ import * as GhostChannels from './GhostChannels';
 
 // Should coincide with channel names mentioned in 'base/main.lua'
 GhostChannels.on('PRINT', json => console.log('[lua]', ...JSON.parse(json)));
-GhostChannels.on('ERROR', json => console.log('[lua]', '[error]', JSON.parse(json).error));
+GhostChannels.on('ERROR', json => {
+  const { error, stacktrace } = JSON.parse(json);
+  console.log('[lua]', '[error]', `${error}\n${stacktrace}`);
+});
