8ea2fb140dfb084be33ac60c6fadf4d9c66d0123
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-01-03 14:52:40 -0500

    mobile: working on getting android building

--- mobile/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ mobile/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "castle://172.20.10.3:19000";
 
 }
