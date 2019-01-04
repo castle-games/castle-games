add1bdaa272671d249e765073eaf26f488398e0f
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-01-04 18:31:31 -0500

    android: seems like we can launch `GameActivity` from expokit

--- mobile/android/app/src/main/java/ghost/GhostViewManager.java
+++ mobile/android/app/src/main/java/ghost/GhostViewManager.java
@@ -1,7 +1,12 @@
 package ghost;
 
+import android.app.Activity;
+import android.content.Intent;
+
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+
+import org.love2d.android.GameActivity;
 
 public class GhostViewManager extends SimpleViewManager<GhostView> {
   public static final String REACT_CLASS = "GhostView";
@@ -13,6 +18,9 @@ public class GhostViewManager extends SimpleViewManager<GhostView> {
 
   @Override
   protected GhostView createViewInstance(ThemedReactContext reactContext) {
+    Activity activity = reactContext.getCurrentActivity();
+    Intent intent = new Intent(activity, GameActivity.class);
+    activity.startActivity(intent);
     return new GhostView(reactContext);
   }
 }
