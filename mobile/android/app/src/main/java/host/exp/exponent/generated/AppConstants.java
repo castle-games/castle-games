cf69b16b602371275b78131e3ca5dded57b4ad75
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-01-11 19:21:55 -0800

    mobile: new publish

--- mobile/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ mobile/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -32,10 +32,11 @@ public class AppConstants {
         
         
         
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@nikki/castle", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40nikki%2Fcastle%2F1.0.0%2Fbd50884f98f0e846423b45e1ec9ee767-30.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40nikki%2Fcastle%2F1.0.0%2Fc028e47762c2b222f6cf8bb1c712b90f-30.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
