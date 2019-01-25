7c2b2786c9081eaae514a55e56cb17f9b37547ea
Ben Roth <spongebucket@gmail.com>
2019-01-24 18:55:31 -0800

    desktop: show basic media info in game action bar

--- desktop/components/GameActionsBar.js
+++ desktop/components/GameActionsBar.js
@@ -7,11 +7,12 @@ import * as SVG from '~/core-components/primitives/svg';
 
 const STYLES_CONTAINER = css`
   background: ${Constants.colors.white};
-  height: 48px;
+  height: 72px;
   flex-shrink: 0;
   width: 100%;
   display: flex;
   align-items: center;
+  flex-direction: column;
 `;
 
 const STYLES_LEFT_ACTIONS = css`
@@ -22,16 +23,26 @@ const STYLES_LEFT_ACTIONS = css`
   display: flex;
 `;
 
-const STYLES_NAME = css`
+const STYLES_NAME_ROW = css`
   color: ${Constants.colors.black};
   font-size: 14pt;
+  display: flex;
+  width: 100%;
+  height: 44px;
+  align-items: center;
+`;
+
+const STYLES_CREATOR_ROW = css`
+  display: flex;
+  width: 100%;
+  font-size: 10pt;
 `;
 
 export default class GameActionsBar extends React.Component {
   render() {
     let muteIcon = (this.props.isMuted) ?
-        <SVG.Mute height="16px" /> :
-        <SVG.Audio height="16px" />;
+        <SVG.Mute height="14px" /> :
+        <SVG.Audio height="14px" />;
     let muteElement = (
       <UIButtonDarkSmall
         icon={muteIcon}
@@ -39,14 +50,34 @@ export default class GameActionsBar extends React.Component {
         style={{ background: Constants.colors.black }}
         />
     );
-    const name = this.props.media ? this.props.media.name : 'Untitled';
+    let name = 'Untitled';
+    let username = 'Anonymous';
+    let isRegistered = false;
+    let { media } = this.props;
+    if (media) {
+      name = (media.name) ? media.name : name;
+      isRegistered = (media.slug && media.user);
+      if (isRegistered) {
+        username = media.user.username;
+      } else {
+        username = media.username ? media.username : username;
+        if (media.user && media.user.username) {
+          // TODO: deprecated: unregistered media shouldn't have a `user`,
+          // so we can just delete this case once that's true.
+          username = media.user.username;
+        }
+      }
+    }
     return (
       <div className={STYLES_CONTAINER}>
-        <div className={STYLES_NAME}>
+        <div className={STYLES_NAME_ROW}>
           {name}
+          <div className={STYLES_LEFT_ACTIONS}>
+            {muteElement}
+          </div>
         </div>
-        <div className={STYLES_LEFT_ACTIONS}>
-          {muteElement}
+        <div className={STYLES_CREATOR_ROW}>
+          Made by {username}
         </div>
       </div>
     );
