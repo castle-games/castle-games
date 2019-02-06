e1c59babe873724a01359fe9cc59634db8b952cf
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-02-06 14:10:56 -0500

    macos: add 'release.sh' script to create 'Castle-$VERSION.pkg'

---
+++ macosx/release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rm -rf archive.xcarchive
+rm -rf pkg-root
+
+VERSION=$(/usr/libexec/PlistBuddy -c 'Print CFBundleShortVersionString' Supporting/ghost-macosx.plist)
+
+xcodebuild -project ghost.xcodeproj -config Release -scheme ghost-macosx -archivePath ./archive archive
+mkdir -p pkg-root/Applications
+mv archive.xcarchive/Products/Applications/Castle.app pkg-root/Applications/
+pkgbuild --root pkg-root --identifier io.playcastle.castle --version $VERSION Castle-$VERSION.pkg
+
+rm -rf archive.xcarchive
+rm -rf pkg-root
+
