5ac77d9ab91af49f3472416c6c526a4f928247bd
Jesse Ruder <jesse@getexponent.com>
2019-02-13 14:25:13 -0800

    send commit hash in upload script

--- ci/upload_script.js
+++ ci/upload_script.js
@@ -1,5 +1,6 @@
 var fs = require("fs");
 var request = require("request");
+var exec = require("child_process").exec;
 
 var tokenFilename =
   process.env["DOWNLOADSECUREFILE_SECUREFILEPATH"] ||
@@ -7,26 +8,30 @@ var tokenFilename =
 
 var token = fs.readFileSync(tokenFilename, "utf8");
 
-request.post(
-  {
-    url: "https://api.castle.games/api/upload-client-release",
-    headers: {
-      "X-Auth-Token": token
+exec("git rev-parse HEAD", function(error, stdout) {
+  stdout = stdout.trim();
+  request.post(
+    {
+      url: "https://api.castle.games/api/upload-client-release",
+      headers: {
+        "X-Auth-Token": token
+      },
+      qs: {
+        platform: process.argv[2],
+        githash: stdout
+      },
+      formData: {
+        file: fs.createReadStream(process.argv[3])
+      }
     },
-    qs: {
-      platform: process.argv[2]
-    },
-    formData: {
-      file: fs.createReadStream(process.argv[3])
+    function(err, resp, body) {
+      if (err) {
+        console.log("Error!");
+        process.exit(1);
+      } else {
+        console.log("Success!");
+        process.exit(0);
+      }
     }
-  },
-  function(err, resp, body) {
-    if (err) {
-      console.log("Error!");
-      process.exit(1);
-    } else {
-      console.log("Success!");
-      process.exit(0);
-    }
-  }
-);
+  );
+});
