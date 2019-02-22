0e5d19fe54b094286b84fe47c0784e54720781c0
Ben Roth <ben@castle.games>
2019-02-21 18:25:09 -0800

    desktop: split app structure into app container
    because jim said it was a good idea

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import { css } from 'react-emotion';
 import { isKeyHotkey } from 'is-hotkey';
 
 import * as Actions from '~/common/actions';
-import * as Constants from '~/common/constants';
+import AppContainer from '~/components/AppContainer';
 import { CurrentUserContextProvider } from '~/contexts/CurrentUserContext';
 import {
   DevelopmentSetterContext,
@@ -16,25 +15,11 @@ import * as NativeUtil from '~/native/nativeutil';
 import { linkify } from 'react-linkify';
 import * as Urls from '~/common/urls';
 
-import ContentContainer from '~/components/ContentContainer.js';
-import SocialContainer from '~/components/SocialContainer.js';
-
 const isReloadHotkey = isKeyHotkey('mod+r');
 const isFullscreenHotkey = isKeyHotkey('mod+shift+f');
 const isDevelopmentHotkey = isKeyHotkey('mod+j');
 
 const NATIVE_CHANNELS_POLL_INTERVAL = 300;
-
-const STYLES_CONTAINER = css`
-  font-family: ${Constants.font.default};
-  background: ${Constants.colors.background};
-  height: 100vh;
-  width: 100%;
-  overflow: hidden;
-  margin: 0;
-  padding: 0;
-  display: flex;
-`;
 
 class App extends React.Component {
   _nativeChannelsPollTimeout;
@@ -135,15 +120,12 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className={STYLES_CONTAINER}>
-        <SocialContainer />
-        <ContentContainer
-          featuredGames={this.state.featuredGames}
-          allContent={this.state.allContent}
-          updateAvailable={this.state.updateAvailable}
-          onNativeUpdateInstall={this._handleNativeUpdateInstall}
-        />
-      </div>
+      <AppContainer
+        featuredGames={this.state.featuredGames}
+        allContent={this.state.allContent}
+        updateAvailable={this.state.updateAvailable}
+        onNativeUpdateInstall={this._handleNativeUpdateInstall}
+      />
     );
   }
 }
