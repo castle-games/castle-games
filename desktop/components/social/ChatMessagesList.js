8aa29aa95f81e157e12bf808369ed4157a7e67dc
Jesse Ruder <jesse@getexponent.com>
2019-02-22 16:40:39 -0800

    add chat avatars and times

--- desktop/components/social/ChatMessagesList.js
+++ desktop/components/social/ChatMessagesList.js
@@ -3,6 +3,7 @@ import { css } from 'react-emotion';
 import { SocialContext } from '~/contexts/SocialContext';
 import Linkify from 'react-linkify';
 import { getEmojiComponent } from '~/common/emojis';
+import UIAvatar from '~/components/reusable/UIAvatar';
 
 import * as Constants from '~/common/constants';
 
@@ -31,14 +32,22 @@ const STYLES_CHAT_ITEM = css`
 `;
 
 const STYLES_MESSAGE_USERNAME = css`
-  padding-top: 8px;
   font-weight: 800;
   cursor: pointer;
+  padding-bottom: 2px;
+`;
+
+const STYLES_MESSAGE_TIME = css`
+  padding-left: 10px;
+  font-weight: 100;
+  font-size: 0.8rem;
+  color: #666;
 `;
 
 const STYLES_MESSAGE = css`
-  padding: 4px 0 4px 16px;
+  padding: 4px 0 4px 32px;
   overflow-wrap: break-word;
+  float: 'left';
 `;
 
 const STYLES_MESSAGE_TAG = css`
@@ -106,6 +115,14 @@ class ChatMessagesList extends React.Component {
     return result;
   };
 
+  _areDatesSameDay = (date1, date2) => {
+    return (
+      date1.getDate() === date2.getDate() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getFullYear() === date2.getFullYear()
+    );
+  };
+
   render() {
     let listItems = [];
     let prevUserId = null;
@@ -113,7 +130,8 @@ class ChatMessagesList extends React.Component {
       // TODO: show UIAvatar on the left along with author name.
       const chatMessage = this.props.messages[ii];
       const userId = chatMessage.userId;
-      let maybeUsername;
+      let maybeAvatar = <div style={{ width: 24, marginRight: 8, float: 'left' }} />;
+      let maybeUsername = null;
       if (!prevUserId || prevUserId !== userId) {
         let isRealUser = !!this.props.social.userIdToUser[chatMessage.userId];
         let user = this.props.social.userIdToUser[chatMessage.userId] || {
@@ -125,18 +143,54 @@ class ChatMessagesList extends React.Component {
           user = { userId: NOTIFICATIONS_USER_ID, username: 'Castle' };
         }
 
+        if (user.photo && user.photo.url) {
+          maybeAvatar = (
+            <UIAvatar
+              src={user.photo.url}
+              style={{
+                width: 24,
+                height: 24,
+                marginRight: 8,
+                marginTop: 8,
+                float: 'left',
+              }}
+            />
+          );
+        }
+
+        let date = new Date(chatMessage.timestamp);
+        let timeString = date.toLocaleString('en-US', {
+          hour: 'numeric',
+          minute: 'numeric',
+          hour12: true,
+        });
+
+        if (!this._areDatesSameDay(new Date(), date)) {
+          let yesterday = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
+
+          if (this._areDatesSameDay(yesterday, date)) {
+            timeString = `Yesterday at ${timeString}`;
+          } else {
+            timeString = `${date.toLocaleString('en-us', {
+              month: 'short',
+            })} ${date.getDate()} at ${timeString}`;
+          }
+        }
+
         maybeUsername = (
-          <div
-            className={STYLES_MESSAGE_USERNAME}
-            onClick={isRealUser ? () => this.props.navigateToUserProfile(user) : null}>
-            {user.username}
+          <div onClick={isRealUser ? () => this.props.navigateToUserProfile(user) : null}>
+            <div className={STYLES_MESSAGE_USERNAME}>
+              {user.username}
+              <span className={STYLES_MESSAGE_TIME}>{timeString}</span>
+            </div>
           </div>
         );
       }
       listItems.push(
         <div key={chatMessage.key} className={STYLES_CHAT_ITEM}>
-          {maybeUsername}
+          {maybeAvatar}
           <div className={STYLES_MESSAGE}>
+            {maybeUsername}
             <Linkify>{this._renderChatMessage(chatMessage.message)}</Linkify>
           </div>
         </div>
