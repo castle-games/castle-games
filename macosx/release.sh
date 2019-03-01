ad3ed4ddc85d214bae7036cc826a96559688ea21
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-02-28 17:53:38 -0800

    macosx: we now use a custom 'generate_appcast' fork that reads env var

--- macosx/release.sh
+++ macosx/release.sh
@@ -34,10 +34,5 @@ fi
 cd castle-releases
 echo "Pulling 'castle-releases'..."
 git pull origin master
-if [ ! -z "$CASTLE_SPARKLE_KEY" ]; then
-  echo "Adding Sparkle key to keychain..."
-  security unlock-keychain -p circle
-  security add-generic-password -a "ed25519" -s "https://sparkle-project.org" -w "$CASTLE_SPARKLE_KEY" -U login.keychain
-fi
 echo "Performing release..."
 ./castle-releases-macos mac ../Castle-$MACOS_VERSION.zip
