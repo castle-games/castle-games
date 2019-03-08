a5d04119e5fb7a8bab6920505f4bce38e2c76040
Ben Roth <ben@castle.games>
2019-03-08 10:49:13 -0800

    fixes castle web url in starter project

--- shared-assets/blank.lua
+++ shared-assets/blank.lua
@@ -1,5 +1,5 @@
 -- Welcome to your new Castle project!
--- https://playcastle.io/get-started
+-- https://castle.games/get-started
 -- Castle uses Love2D 11.1 for rendering and input: https://love2d.org/
 -- See here for some useful Love2D documentation:
 -- https://love2d-community.github.io/love-api/
