a50c92f30e78b58a55257155254e0242989747d4
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-03-20 13:02:14 -0700

    actually cleanly build on windows ci

--- megasource/release.sh
+++ megasource/release.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-# if [[ -n $(git status --porcelain) ]]; then
-#   echo "Tree is dirty, aborting"
-#   exit 1
-# fi
+if [[ -n $(git status --porcelain) ]]; then
+  echo "Tree is dirty, aborting"
+  exit 1
+fi
 
 git fetch --tags --prune
 GIT_HASH=$(git rev-parse HEAD)
 WIN_BASE_VERSION=1
 WIN_VERSION=$WIN_BASE_VERSION.$(git rev-list release-root..HEAD --count)
 
-# rm -rf build/Release
-# ./run_cmake.sh
+rm -rf build/Release
+./run_cmake.sh
 cmake.exe --build build --config Release
 
 echo -e "\n\b\bCreated 'Castle-$WIN_VERSION.zip'"
