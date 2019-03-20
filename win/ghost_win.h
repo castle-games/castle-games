a64c00b89042b00318e20f94c9edfd1c0f34cfe2
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-03-19 20:15:49 -0700

    win: prepare to use actual releases endpoint for auto-updates

--- win/ghost_win.h
+++ win/ghost_win.h
@@ -4,4 +4,4 @@ HWND ghostWinGetChildWindow();
 }
 
 // #define GHOST_WIN_UPDATES_URL L"C:\\Users\\nikki\\Development\\ghost\\megasource\\castle-releases\\win"
-#define GHOST_WIN_UPDATES_URL L"https://raw.githubusercontent.com/castle-games/castle-releases/win2/win"
+#define GHOST_WIN_UPDATES_URL L"https://api.castle.games/api/releases/win/win"
