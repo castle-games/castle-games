b43f85cd311a3881907ad32897213fdce2196a21
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-03-19 19:26:25 -0700

    use remote 'castle-releases' branch

--- win/ghost_win.h
+++ win/ghost_win.h
@@ -3,5 +3,5 @@ HWND ghostWinGetMainWindow();
 HWND ghostWinGetChildWindow();
 }
 
-#define GHOST_WIN_UPDATES_URL L"C:\\Users\\nikki\\Development\\ghost\\megasource\\castle-releases\\win"
-// #define GHOST_WIN_UPDATES_URL L"https://raw.githubusercontent.com/castle-games/castle-releases/win2/win"
+// #define GHOST_WIN_UPDATES_URL L"C:\\Users\\nikki\\Development\\ghost\\megasource\\castle-releases\\win"
+#define GHOST_WIN_UPDATES_URL L"https://raw.githubusercontent.com/castle-games/castle-releases/win2/win"
