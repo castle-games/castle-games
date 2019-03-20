1b611e46da4031eabd58553ec6f4bfb898e160ec
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-03-20 14:54:56 -0700

    make 'run_cmake.sh' able to run in 'git bash'

--- megasource/run_cmake.sh
+++ megasource/run_cmake.sh
@@ -1,4 +1,8 @@
+case "$(uname -s)" in
+	Linux*) mdFlag='/MD';;
+	MINGW*) mdFlag='//MD';;
+esac
 cmake.exe -DMEGA_LOVE=../love \
 	-DOPENSSL_ROOT_DIR=../openssl/openssl-1.0.2l-vs2017 -DOPENSSL_USE_STATIC_LIBS=TRUE -DOPENSSL_MSVC_STATIC_RT=TRUE \
-	-DCEF_RUNTIME_LIBRARY_FLAG='/MD' \
+	-DCEF_RUNTIME_LIBRARY_FLAG=$mdFlag \
 	-G "Visual Studio 15" -H. -Bbuild
