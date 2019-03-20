e4219981f440af495c908e5c4fdb1c61961af008
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-03-20 12:47:53 -0700

    update windows 'release.sh' to prepare for ci

--- megasource/release.sh
+++ megasource/release.sh
@@ -1,9 +1,29 @@
+# Build and upload a new version of Castle for Windows
+
 set -e
-rm -rf build/Release
-./run_cmake.sh
+
+# if [[ -n $(git status --porcelain) ]]; then
+#   echo "Tree is dirty, aborting"
+#   exit 1
+# fi
+
+git fetch --tags --prune
+GIT_HASH=$(git rev-parse HEAD)
+WIN_BASE_VERSION=1
+WIN_VERSION=$WIN_BASE_VERSION.$(git rev-list release-root..HEAD --count)
+
+# rm -rf build/Release
+# ./run_cmake.sh
 cmake.exe --build build --config Release
 
-cd installer
-cmake.exe -H. -Bbuild
-cmake.exe --build build --target PACKAGE_AND_MOVE --config Release
-cd ..
+echo -e "\n\b\bCreated 'Castle-$WIN_VERSION.zip'"
+
+if [ ! -d castle-releases ]; then
+  echo "Cloning 'castle-releases'..."
+  git clone https://$CASTLE_GITHUB_TOKEN@github.com/castle-games/castle-releases.git
+fi
+cd castle-releases
+echo "Pulling 'castle-releases'..."
+git pull origin master
+echo "Performing release..."
+./castle-releases-win.exe win ../build/Release $WIN_VERSION ../extra/castle.ico
