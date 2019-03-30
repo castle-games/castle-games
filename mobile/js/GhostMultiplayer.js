8916907b319c698269dcf37f2a62e3de5f537d0b
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-03-30 12:59:42 -0700

    mobile: use `joinMultiplayerSession`

--- mobile/js/GhostMultiplayer.js
+++ mobile/js/GhostMultiplayer.js
@@ -7,18 +7,20 @@ GhostChannels.on('CASTLE_CONNECT_MULTIPLAYER_CLIENT_REQUEST', async requestJson 
   const { data } = await apolloClient.mutate({
     mutation: gql`
       mutation($mediaUrl: String!) {
-        multiplayerJoin(mediaUrl: $mediaUrl)
+        joinMultiplayerSession(mediaUrl: $mediaUrl) {
+          address
+        }
       }
     `,
     variables: { mediaUrl },
   });
-  if (data && data.multiplayerJoin) {
+  if (data && data.joinMultiplayerSession) {
     GhostChannels.pushAsync(
       'JS_EVENTS',
       JSON.stringify({
         name: 'CASTLE_CONNECT_MULTIPLAYER_CLIENT_RESPONSE',
         params: {
-          address: data.multiplayerJoin.address,
+          address: data.joinMultiplayerSession.address,
         },
       })
     );
