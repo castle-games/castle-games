3af9860b6f93f2bb4ebb9e7d1e42ab2a0c78c5b3
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-04-08 12:09:59 -0700

    base: formatting

--- base/main.lua
+++ base/main.lua
@@ -325,45 +325,45 @@ end
 
 -- Based on default in https://love2d.org/wiki/love.run with Ghost adjustments
 function love.run()
-	if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
+    if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
 
-	-- We don't want the first frame's dt to include time taken by love.load.
-	if love.timer then love.timer.step() end
+    -- We don't want the first frame's dt to include time taken by love.load.
+    if love.timer then love.timer.step() end
 
-	local dt = 0
+    local dt = 0
 
-	-- Main loop time.
-	return function()
-		-- Process events.
-		if love.event then
-			love.event.pump()
-			for name, a,b,c,d,e,f in love.event.poll() do
-				if name == "quit" then
-					if not love.quit or not love.quit() then
-						return a or 0
-					end
-				end
-				love.handlers[name](a,b,c,d,e,f)
-			end
-		end
+    -- Main loop time.
+    return function()
+        -- Process events.
+        if love.event then
+            love.event.pump()
+            for name, a,b,c,d,e,f in love.event.poll() do
+                if name == "quit" then
+                    if not love.quit or not love.quit() then
+                        return a or 0
+                    end
+                end
+                love.handlers[name](a,b,c,d,e,f)
+            end
+        end
 
-		-- Update dt, as we'll be passing it to update
-		if love.timer then dt = love.timer.step() end
+        -- Update dt, as we'll be passing it to update
+        if love.timer then dt = love.timer.step() end
 
-		-- Call update and draw
-		if love.update then love.update(dt) end -- will pass 0 if love.timer is disabled
+        -- Call update and draw
+        if love.update then love.update(dt) end -- will pass 0 if love.timer is disabled
 
-		if love.graphics and love.graphics.isActive() then
-			love.graphics.origin()
-			love.graphics.clear(love.graphics.getBackgroundColor())
+        if love.graphics and love.graphics.isActive() then
+            love.graphics.origin()
+            love.graphics.clear(love.graphics.getBackgroundColor())
 
-			if love.draw then love.draw() end
+            if love.draw then love.draw() end
 
-			love.graphics.present()
+            love.graphics.present()
         elseif not CASTLE_SERVER then -- XXX(Ghost): Limit framerate when not drawing
             love.timer.sleep(0.016)
         end
 
-		if love.timer then love.timer.sleep(0.001) end
-	end
+        if love.timer then love.timer.sleep(0.001) end
+    end
 end
