5437ee1522d84a9d750b5df5a731fdfdcb15c9fb
Ben Roth <spongebucket@gmail.com>
2019-05-03 10:28:28 -0700

    desktop: make user search results show name in addition to username

--- desktop/components/reusable/UIUserGrid.js
+++ desktop/components/reusable/UIUserGrid.js
@@ -9,7 +9,42 @@ const STYLES_CONTAINER = css`
   display: flex;
   flex-wrap: wrap;
   max-width: 960px;
-  padding: 24px;
+`;
+
+const STYLES_ITEM = css`
+  width: 204px;
+  margin: 24px;
+  background-color: ${Constants.colors.backgroundNavigation};
+  border-radius: 4px;
+  padding: 8px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.4);
+`;
+
+const STYLES_AVATAR = css`
+  width: 90px;
+  height: 90px;
+  background-size: cover;
+  background-position: 50% 50%;
+  border-radius: 4px;
+  cursor: pointer;
+`;
+
+const STYLES_USER_NAME = css`
+  font-family: ${Constants.font.system};
+  color: ${Constants.colors.white};
+  font-weight: 700;
+  margin-top: 8px;
+  cursor: pointer;
+`;
+
+const STYLES_USER_USERNAME = css`
+  color: ${Constants.colors.white};
+  text-transform: uppercase;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  font-size: 11px;
+  line-height: ${Constants.linescale.lvl6};
+  cursor: pointer;
 `;
 
 export default class UIUserGrid extends React.Component {
@@ -19,12 +54,21 @@ export default class UIUserGrid extends React.Component {
       <div className={STYLES_CONTAINER}>
         {users.map((u) => {
           return (
-            <UICharacterCard
-              key={u.userId}
-              user={u}
-              style={{ margin: `0 16px 16px 0` }}
-              onAvatarClick={() => this.props.onUserSelect(u)}
-            />
+            <div key={u.userId} className={STYLES_ITEM}>
+              <div
+                className={STYLES_AVATAR}
+                onClick={() => this.props.onUserSelect(u)}
+                style={{
+                  backgroundImage: u.photo && u.photo.url ? `url(${u.photo.url})` : null,
+                }}
+              />
+              <div className={STYLES_USER_NAME} onClick={() => this.props.onUserSelect(u)}>
+                {u.name}
+              </div>
+              <div className={STYLES_USER_USERNAME} onClick={() => this.props.onUserSelect(u)}>
+                @{u.username}
+              </div>
+            </div>
           );
         })}
       </div>
