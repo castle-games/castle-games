0462446a1fefa0dc697c95865a2bd3be87622080
@wwwjim <send@jimmy.email>
2019-05-22 17:24:53 -0700

    desktop: adds header

--- desktop/components/chat/ChatHeader.js
+++ desktop/components/chat/ChatHeader.js
@@ -1,19 +1,57 @@
-import ReactDOM from 'react-dom';
-
 import * as React from 'react';
 import * as Constants from '~/common/constants';
 
 import { css } from 'react-emotion';
 
-const STYLES_CONTAINER = css`
-  height: 64px;
+const STYLES_HEADER = css`
   border-bottom: 1px solid ${Constants.REFACTOR_COLORS.elements.border};
+  color: ${Constants.REFACTOR_COLORS.text};
+  font-family: ${Constants.REFACTOR_FONTS.system};
+  width: 100%;
   flex-shrink: 0;
+  padding: 16px 16px 8px 16px;
+  overflow: hidden;
+  min-height: 64px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const STYLES_HEADER_LEFT = css`
+  min-width: 25%;
   width: 100%;
 `;
 
-export default class ChatHeader extends React.Component {
+const STYLES_HEADER_RIGHT = css`
+  flex-shrink: 0;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const STYLES_H2 = css`
+  font-size: 16px;
+`;
+
+const STYLES_P = css`
+  margin-top: 4px;
+  font-size: 12px;
+  cursor: pointer;
+`;
+
+export default class ClientChatWindow extends React.Component {
   render() {
-    return <div className={STYLES_CONTAINER}>ChatHeader</div>;
+    return (
+      <header className={STYLES_HEADER}>
+        <div className={STYLES_HEADER_LEFT}>
+          <h2 className={STYLES_H2}>#[bind-channel-name]</h2>
+          <p className={STYLES_P}>
+            <strong style={{ cursor: 'pointer' }}>[bind-member-count] online</strong>
+          </p>
+        </div>
+        <div className={STYLES_HEADER_RIGHT}>Test</div>
+      </header>
+    );
   }
 }
