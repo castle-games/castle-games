1aee04caea824b64d2c409e8dfb587bd53dee459
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-05-28 21:07:04 -0700

    desktop: maybe fix css loading for mac

--- desktop/webpack.config.js
+++ desktop/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCSSExtractPlugin.loader, 'css-loader'],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpg|gif)$/,
