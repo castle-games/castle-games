51452c76157a8f0436103e6590e5a6bb22540ddc
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-06-10 11:06:04 -0700

    base: add missed `CASTLE_INITIAL_DATA == nil` test

--- base/main.lua
+++ base/main.lua
@@ -296,7 +296,7 @@ do
     -- Wrap this to account for our own dimensions stuff
     local oldW, oldH
     function main.resize()
-        if home and CASTLE_INITIAL_DATA.graphics.width == 0 then -- Only send resize events in 'full' case
+        if home and CASTLE_INITIAL_DATA and CASTLE_INITIAL_DATA.graphics.width == 0 then -- Only send resize events in 'full' case
             local w, h = home.globals.love.graphics.getDimensions()
             if w ~= oldW or h ~= oldH then
                 home:resize(w, h)
