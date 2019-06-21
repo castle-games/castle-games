98d14a046957302f8a914a12e434509e22f49d11
Ben Roth <spongebucket@gmail.com>
2019-06-21 11:14:33 -0700

    macos: fix update framework script to limit to only Versions/A

--- macosx/tools/update-framework.sh
+++ macosx/tools/update-framework.sh
@@ -1,37 +1,41 @@
 #!/bin/sh
-# usage: update-framework [path] [lib name]
+# usage: update-framework [path]
 #
-# e.g. update-framework ./path/to/FreeType.framework FreeType
+# e.g. update-framework ./path/to/FreeType.framework
 #
 # WARNING: recommend doing this from someplace version controlled, because it deletes files.
 # 
 # fixes a pre-mavericks macOS framework to follow the convention specified here:
 # https://developer.apple.com/library/archive/technotes/tn2206/_index.html#//apple_ref/doc/uid/DTS40007919-CH1-TNTAG201
 #
-# assumes you have castle's cert in your keychain.
-# Upon success you should see something like:
-# > signed bundle with Mach-O thin (x86_64)
 
 set -e
 
 if [ -z "$1" ]
 then
-    echo "#usage: update-framework [path] [lib name]"
+    echo "#usage: update-framework [path]"
     exit 1
 fi
 
-if [ ! git rev-parse --git-dir > /dev/null 2>&1; ]
-then
-   echo "not inside a git repo, exiting"
-   exit 1
+if git rev-parse --git-dir > /dev/null 2>&1; then
+    echo "inside a git repo, continuing"
+else
+    echo "not inside a git repo, exiting"
+    exit 1
 fi
 
+pushd $1
+
+echo "ln -s Versions/A Versions/Current"
+pushd Versions
+rm -rf Current
+ln -s A Current
+popd
+
 echo "Removing invalid root level stuff"
-find $1 ! -path "./Versions" ! -path . -maxdepth 1 -exec rm -r {} \;
+find . ! -path "./Versions" ! -path . -maxdepth 1 -exec rm -r {} \;
 
 echo "ln -s Versions/Current/* ."
 ln -s Versions/Current/* .
 
-echo "Codesigning new contents"
-DEVELOPER_IDENTITY=$(security find-identity -v -p codesigning | grep -o "Developer ID Application: Castle Games, Inc\. (.*)")
-codesign --verbose --deep -s "${DEVELOPER_IDENTITY}" Versions/Current/$2
+popd
