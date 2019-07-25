523355dd06ddd5ddd53310733ab3c30117b2fe3a
Ben Roth <spongebucket@gmail.com>
2019-07-25 13:42:09 -0700

    desktop: clean up initial app state

---
+++ desktop/src/globalStyles.js
@@ -0,0 +1,71 @@
+import * as Constants from '~/common/constants';
+
+import { injectGlobal } from 'react-emotion';
+
+export const injectGlobalStyles = () => injectGlobal`
+  @font-face {
+    font-family: 'game-heading';
+    src: url('static/Font-Logo-RTAliasBold.woff');
+  }
+  @font-face {
+    font-family: 'logo-heading';
+    src: url('static/Font-Logo2-RTAliasMedium.woff');
+  }
+  @font-face {
+    font-family: 'sf-heading';
+    src: url('static/Font-Headings-SFDisplay.woff');
+  }
+  @font-face {
+    font-family: 'sf-body';
+    src: url('static/Font-Body-SFDisplay.woff');
+  }
+  @font-face {
+    font-family: 'sf-mono';
+    src: url('static/Font-Mono-SFMono.woff');
+  }
+  @font-face {
+    font-family: 'sf-mono-bold';
+    src: url('static/SFMono-Bold.woff');
+  }
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    vertical-align: baseline;
+  }
+
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+
+  html, body {
+    font-family: ${Constants.font.default};
+    background: ${Constants.colors.black};
+    font-size: 16px;
+  }
+`;
+
+export const injectGlobalScrollOverflowPreventionStyles = () => injectGlobal`
+  html {
+    overflow: hidden;
+  }
+
+  body {
+    overflow: auto;
+    -webkit-overflow-scrolling: touch;
+  }
+`;
--- desktop/src/index.js
+++ desktop/src/index.js
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import * as Constants from '~/common/constants';
-import * as Network from '~/common/network';
 import * as Actions from '~/common/actions';
 import * as Analytics from '~/common/analytics';
 
-import { injectGlobal } from 'react-emotion';
+import { injectGlobalStyles, injectGlobalScrollOverflowPreventionStyles } from './globalStyles';
 import { injectGlobalLoaderStyles } from '~/components/primitives/loader';
 
 import ReactDOM from 'react-dom';
@@ -14,74 +12,6 @@ import Storage from '~/common/storage';
 
 import 'react-tippy/dist/tippy.css';
 
-const injectGlobalStyles = () => injectGlobal`
-  @font-face {
-    font-family: 'game-heading';
-    src: url('static/Font-Logo-RTAliasBold.woff');
-  }
-  @font-face {
-    font-family: 'logo-heading';
-    src: url('static/Font-Logo2-RTAliasMedium.woff');
-  }
-  @font-face {
-    font-family: 'sf-heading';
-    src: url('static/Font-Headings-SFDisplay.woff');
-  }
-  @font-face {
-    font-family: 'sf-body';
-    src: url('static/Font-Body-SFDisplay.woff');
-  }
-  @font-face {
-    font-family: 'sf-mono';
-    src: url('static/Font-Mono-SFMono.woff');
-  }
-  @font-face {
-    font-family: 'sf-mono-bold';
-    src: url('static/SFMono-Bold.woff');
-  }
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed,
-  figure, figcaption, footer, header, hgroup,
-  menu, nav, output, ruby, section, summary,
-  time, mark, audio, video {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    border: 0;
-    vertical-align: baseline;
-  }
-
-  article, aside, details, figcaption, figure,
-  footer, header, hgroup, menu, nav, section {
-    display: block;
-  }
-
-  html, body {
-    font-family: ${Constants.font.default};
-    background: ${Constants.colors.black};
-    font-size: 16px;
-  }
-`;
-
-const injectGlobalScrollOverflowPreventionStyles = () => injectGlobal`
-  html {
-    overflow: hidden;
-  }
-
-  body {
-    overflow: auto;
-    -webkit-overflow-scrolling: touch;
-  }
-`;
-
 const storage = new Storage('castle');
 
 const loader = document.createElement('div');
@@ -89,10 +19,50 @@ loader.id = 'loader';
 
 document.body.appendChild(loader);
 
-const INITIAL_STATE_OFFLINE = {
-  trendingGames: [],
-  featuredExamples: [],
-  isOffline: true,
+const mountLoader = () => {
+  ReactDOM.render(<GLLoaderScreen />, document.getElementById('loader'));
+};
+
+const unmountLoader = async () => {
+  document.getElementById('loader').classList.add('loader--finished');
+  await Actions.delay(1000);
+  ReactDOM.unmountComponentAtNode(document.getElementById('loader'));
+  document.getElementById('loader').outerHTML = '';
+};
+
+const getInitialState = async () => {
+  let data;
+  let trendingGames = [];
+  let featuredExamples = [];
+  let currentUser = {};
+  let isOffline = true;
+
+  try {
+    data = await Actions.getInitialData();
+  } catch (e) {
+    console.log(`Issue fetching initial Castle data: ${e}`);
+  }
+
+  if (data) {
+    isOffline = false;
+    trendingGames = data.trendingGames ? data.trendingGames : [];
+    featuredExamples = data.featuredExamples ? data.featuredExamples : [];
+    currentUser = {
+      user: data.me,
+      settings: {
+        notifications: data.getNotificationPreferences,
+      },
+      userStatusHistory: data.userStatusHistory,
+    };
+  }
+
+  return {
+    trendingGames,
+    featuredExamples,
+    currentUser,
+    isOffline,
+    navigation: currentUser.user ? { contentMode: 'home' } : { contentMode: 'signin' },
+  };
 };
 
 const run = async () => {
@@ -100,39 +70,16 @@ const run = async () => {
   await Analytics.initialize();
   Analytics.trackCastleLaunch();
 
-  ReactDOM.render(<GLLoaderScreen />, document.getElementById('loader'));
-  const {
-    trendingGames,
-    featuredExamples,
-    currentUser,
-    isOffline,
-  } = await Network.getProductData();
+  mountLoader();
+  let state = await getInitialState();
 
   // if the user was automatically logged in when starting Castle, track that
-  if (currentUser && currentUser.user) {
-    Analytics.trackLogin({ user: currentUser.user, isAutoLogin: true });
+  if (state.currentUser && state.currentUser.user) {
+    Analytics.trackLogin({ user: state.currentUser.user, isAutoLogin: true });
   }
 
-  let state = Object.assign({}, INITIAL_STATE_OFFLINE, {
-    trendingGames,
-    featuredExamples,
-    currentUser,
-    isOffline,
-  });
-
-  // NOTE(jim): You must be authenticated to use Castle.
-  // https://github.com/castle-games/ghost/issues/480
-  state.navigation = currentUser.user ? { contentMode: 'home' } : { contentMode: 'signin' };
-
   ReactDOM.render(<App state={state} storage={storage} />, document.getElementById('root'));
-
-  document.getElementById('loader').classList.add('loader--finished');
-
-  await Actions.delay(1000);
-
-  ReactDOM.unmountComponentAtNode(document.getElementById('loader'));
-
-  document.getElementById('loader').outerHTML = '';
+  await unmountLoader();
 };
 
 injectGlobalStyles();
