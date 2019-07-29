53300f5b45d8d1d3a65e7fd85f83e3b35301bf04
Ben Roth <spongebucket@gmail.com>
2019-07-29 13:54:55 -0700

    desktop: add scrolling category navigation to emoji picker

--- desktop/components/chat/ChatInputEmojiPicker.js
+++ desktop/components/chat/ChatInputEmojiPicker.js
@@ -9,7 +9,6 @@ const STYLES_CONTAINER = css`
   bottom: 56px;
   right: 16px;
   width: 384px;
-  height: 256px;
 
   padding: 8px;
 
@@ -17,7 +16,10 @@ const STYLES_CONTAINER = css`
   border: 1px solid rgba(219, 219, 219, 1);
   background: #fff;
   box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1);
+`;
 
+const STYLES_SCROLLING_CONTAINER = css`
+  height: 256px;
   overflow-y: scroll;
 
   ::-webkit-scrollbar {
@@ -55,30 +57,89 @@ const STYLES_CATEGORY_LIST = css`
   flex-wrap: wrap;
 `;
 
+const STYLES_NAV_CONTAINER = css`
+  display: flex;
+  border-bottom: 1px solid #ececec;
+  margin-bottom: 8px;
+`;
+
+const STYLES_NAV_ITEM = css`
+  cursor: pointer;
+  border-radius: 3px;
+  padding: 4px;
+  margin: 0 4px 4px 4px;
+
+  :hover {
+    background: #ececec;
+  }
+`;
+
+const CATEGORY_HEADER_EMOJI = [
+  'grinning',
+  'leaves',
+  'hamburger',
+  'airplane',
+  'soccer',
+  'bulb',
+  'arrow_right',
+  'waving_black_flag',
+  'skin-tone-4',
+];
+
 export default class ChatInputEmojiPicker extends React.Component {
+  _categoryHeaderRefs = {};
+
   static defaultProps = {
     onSelectEmoji: (shortName) => {},
+  };
+
+  _scrollToCategoryIndex = (index) => {
+    if (this._categoryHeaderRefs[index]) {
+      this._categoryHeaderRefs[index].scrollIntoView();
+    }
+  };
+
+  _renderCategoryNavigation = (categories) => {
+    return (
+      <div className={STYLES_NAV_CONTAINER}>
+        {categories.map((category, ii) => (
+          <div
+            className={STYLES_NAV_ITEM}
+            key={`category-nav-${ii}`}
+            onClick={() => this._scrollToCategoryIndex(ii)}>
+            {getEmojiComponent(CATEGORY_HEADER_EMOJI[ii])}
+          </div>
+        ))}
+      </div>
+    );
   };
 
   render() {
     const categories = getCategories();
     return (
       <div className={STYLES_CONTAINER}>
-        {categories.map((category, ii) => (
-          <div className={STYLES_CATEGORY_SECTION} key={`category-${ii}`}>
-            <div className={STYLES_CATEGORY_TITLE}>{category.title}</div>
-            <div className={STYLES_CATEGORY_LIST}>
-              {category.emojis.map((short_name, jj) => (
-                <div
-                  key={`emoji-${jj}`}
-                  className={STYLES_EMOJI_ITEM}
-                  onClick={() => this.props.onSelectEmoji(short_name, true)}>
-                  {getEmojiComponent(short_name)}
-                </div>
-              ))}
+        {this._renderCategoryNavigation(categories)}
+        <div className={STYLES_SCROLLING_CONTAINER}>
+          {categories.map((category, ii) => (
+            <div className={STYLES_CATEGORY_SECTION} key={`category-${ii}`}>
+              <div
+                className={STYLES_CATEGORY_TITLE}
+                ref={(c) => (this._categoryHeaderRefs[ii] = c)}>
+                {category.title}
+              </div>
+              <div className={STYLES_CATEGORY_LIST}>
+                {category.emojis.map((short_name, jj) => (
+                  <div
+                    key={`emoji-${jj}`}
+                    className={STYLES_EMOJI_ITEM}
+                    onClick={() => this.props.onSelectEmoji(short_name, true)}>
+                    {getEmojiComponent(short_name)}
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     );
   }
