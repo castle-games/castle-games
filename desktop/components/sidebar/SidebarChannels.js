f6e7cc36abf6bf8d61b36a9d9dc69209ddfb6499
Ayla Myers <bridgs.dev@gmail.com>
2019-07-30 19:20:48 -0400

    Hiding Recently Played channels under Show more

--- desktop/components/sidebar/SidebarChannels.js
+++ desktop/components/sidebar/SidebarChannels.js
@@ -11,10 +11,47 @@ const STYLES_CONTAINER = css`
   margin-bottom: 24px;
 `;
 
+const STYLES_TOGGLE_MORE_LINK = css`
+  font-family: ${Constants.REFACTOR_FONTS.system};
+  padding: 0 16px 0 16px;
+  text-decoration: underline;
+  cursor: pointer;
+  font-size: 14px;
+  color: ${Constants.colors.text2};
+
+  :hover {
+    color: ${Constants.colors.brand2};
+  }
+`;
+
 export default class SidebarChannels extends React.Component {
   static defaultProps = {
     channels: [],
     filterChannel: (channel) => true,
+  };
+
+  state = {
+    showingMore: false,
+  };
+
+  _handleToggleMore = () => {
+    this.setState((prevState, props) => ({
+      showingMore: !prevState.showingMore,
+    }));
+  };
+
+  // Returns the time of the most recent activity for the channel. Join and leave notifications do not count
+  _getTimeOfMostRecentChannelActivity = (channel) => {
+    if (channel.messages) {
+      for (let i = channel.messages.length - 1; i >= 0; i--) {
+        let message = channel.messages[i];
+        if (!message.body || (message.body.notificationType !== 'joined-channel' && message.body.notificationType !== 'left-channel')) {
+          let date = new Date(message.timestamp);
+          return date.getTime();
+        }
+      }
+    }
+    return 0;
   };
 
   render() {
@@ -25,16 +62,46 @@ export default class SidebarChannels extends React.Component {
         filteredChannels.push(channel);
       }
     });
-    filteredChannels = ChatUtilities.sortChannels(filteredChannels);
 
     if (!filteredChannels.length) {
       return null;
     }
 
+    // Of the filtered channels, figure out which ones are visible
+    let visibleChannels;
+    // If we're showing all the channels or don't have many to begin with, show all of them
+    if (this.state.showingMore || filteredChannels.length <= 5) {
+      visibleChannels = filteredChannels;
+    // Otherwise we display channels with high activity
+    } else {
+      // Sort by activity
+      let channelsSortedByActivity = filteredChannels.sort((a, b) => {
+        return this._getTimeOfMostRecentChannelActivity(b) - this._getTimeOfMostRecentChannelActivity(a);
+      });
+      visibleChannels = [];
+      let now = Date.now();
+      let twentyFourHours = 24 * 60 * 60 * 1000;
+      for (let c of channelsSortedByActivity) {
+        let hasVeryRecentActivity = this._getTimeOfMostRecentChannelActivity(c) > now - twentyFourHours;
+        let isSelectedChannel = c.channelId === this.props.selectedChannelId && this.props.isChatVisible;
+        let hasNotifications = c.unreadNotificationCount > 0;
+        // Always display the 5 most active channels, any channels with messages in the past day,
+        //  the selected channel, and any channels with unread notifications
+        if (visibleChannels.length < 5 || hasVeryRecentActivity || isSelectedChannel || hasNotifications) {
+          visibleChannels.push(c);
+        }
+      }
+    }
+
+    // Sort the list of visible channels
+    visibleChannels = ChatUtilities.sortChannels(visibleChannels);
+
+    let showToggleMore = this.state.showingMore || visibleChannels.length < filteredChannels.length;
+
     return (
       <div className={STYLES_CONTAINER}>
         <SidebarGroupHeader>{this.props.title}</SidebarGroupHeader>
-        {filteredChannels.map((c) => {
+        {visibleChannels.map((c) => {
           const isSelected =
             c.channelId === this.props.selectedChannelId && this.props.isChatVisible;
 
@@ -47,6 +114,11 @@ export default class SidebarChannels extends React.Component {
             />
           );
         })}
+        {showToggleMore && (
+          <div className={STYLES_TOGGLE_MORE_LINK} onClick={this._handleToggleMore}>
+            {this.state.showingMore ? 'Show less' : 'Show more'}
+          </div>
+        )}
       </div>
     );
   }
