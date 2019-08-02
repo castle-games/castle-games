0eb661c41ac79f27c6baeb9aec76014eda103631
Ben Roth <spongebucket@gmail.com>
2019-08-02 14:16:18 -0700

    desktop: sidebar: suggest more online users if you have no DM threads open

--- desktop/components/sidebar/SidebarDirectMessages.js
+++ desktop/components/sidebar/SidebarDirectMessages.js
@@ -11,7 +11,38 @@ const STYLES_CONTAINER = css`
   margin-bottom: 16px;
 `;
 
+const SUGGEST_MORE_ONLINE_USERS = true;
+const MIN_NUM_DIRECT_MESSAGES = 5;
+
 export default class SidebarDirectMessages extends React.Component {
+  _suggestMoreOnlineUsers = (directMessages) => {
+    const { viewer } = this.props;
+    const { onlineUserIds, userIdToUser } = this.props.userPresence;
+
+    // if you haven't started any DM threads, fill the list with some online users
+    let userIdsInList = {};
+    directMessages.forEach((channel) => (userIdsInList[channel.otherUserId] = true));
+
+    let onlineUserIdIndex = 0;
+    let onlineUserIdsList = Object.keys(onlineUserIds);
+    while (
+      onlineUserIdIndex < onlineUserIdsList.length &&
+      directMessages.length < MIN_NUM_DIRECT_MESSAGES
+    ) {
+      let userId = onlineUserIdsList[onlineUserIdIndex];
+      if (!userIdsInList[userId] && viewer.userId !== userId) {
+        let user = userIdToUser[userId];
+        directMessages.push({
+          channelId: null,
+          otherUserId: userId,
+          otherUserIsOnline: true,
+        });
+        userIdsInList[userId] = true;
+      }
+      onlineUserIdIndex++;
+    }
+  };
+
   render() {
     const { channels, viewer } = this.props;
     if (!viewer) {
@@ -28,11 +59,16 @@ export default class SidebarDirectMessages extends React.Component {
         });
       }
     });
+
+    if (SUGGEST_MORE_ONLINE_USERS && directMessages.length < MIN_NUM_DIRECT_MESSAGES) {
+      this._suggestMoreOnlineUsers(directMessages);
+    }
+
     directMessages = ChatUtilities.sortChannels(directMessages);
 
     return (
       <div className={STYLES_CONTAINER}>
-        {directMessages.map((c) => {
+        {directMessages.map((c, ii) => {
           const isSelected =
             c.channelId === this.props.selectedChannelId && this.props.isChatVisible;
 
@@ -43,9 +79,15 @@ export default class SidebarDirectMessages extends React.Component {
 
           let { status } = UserStatus.renderStatusText(user.lastUserStatus);
 
+          let onClick;
+          if (c.channelId) {
+            onClick = () => this.props.onSelectChannel(c);
+          } else {
+            onClick = () => this.props.onSendMessage(user);
+          }
           return (
             <SidebarMessageItem
-              key={`direct-message-${c.channelId}-${c.otherUserId}`}
+              key={`direct-message-${ii}-${c.otherUserId}`}
               name={user.username}
               isUnread={c.hasUnreadMessages}
               notificationCount={c.unreadNotificationCount}
@@ -53,7 +95,7 @@ export default class SidebarDirectMessages extends React.Component {
               isSelected={isSelected}
               status={status}
               avatarUrl={user.photo ? user.photo.url : null}
-              onClick={() => this.props.onSelectChannel(c)}
+              onClick={onClick}
             />
           );
         })}
