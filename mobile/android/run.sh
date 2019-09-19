3861e9916aad6e51f6e787b3a402546551e3da58
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-09-18 18:13:40 -0700

    mobile: improve running gradle from wsl on windows

--- mobile/android/run.sh
+++ mobile/android/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-./gradlew ${1:-installDevMinSdkDevKernelDebug} --stacktrace && adb shell am start -n io.expo.castle/host.exp.exponent.MainActivity
+GRADLE_COMMAND="./gradlew"
+if grep -q Microsoft /proc/version; then
+    GRADLE_COMMAND="cmd.exe /c gradlew.bat"
+fi
+
+$GRADLE_COMMAND ${1:-installDevMinSdkDevKernelDebug} --stacktrace && adb shell am start -n io.expo.castle/host.exp.exponent.MainActivity
