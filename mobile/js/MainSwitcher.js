94eb0e5b98c2cde8f9f200eb7a4e35f0f08f357f
David Cole <dacole@gmail.com>
2019-10-03 15:32:30 -0700

    functioning game switcher

--- mobile/js/MainSwitcher.js
+++ mobile/js/MainSwitcher.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 import { RootNavigator } from './Navigation';
 import GameScreen from './GameScreen';
@@ -7,19 +7,28 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export let switchTo = () => {};
 
-const Page = ({ visible, children }) => (
-  <View
-    style={{
-      position: 'absolute',
-      left: 0,
-      top: 0,
-      right: 0,
-      bottom: 0,
-      zIndex: visible ? 20 : 10,
-    }}>
-    {children}
-  </View>
-);
+const styles = StyleSheet.create({
+  fullscreen: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1,
+  },
+  windowed: {
+    position: 'absolute',
+    bottom: 64,
+    left: 16,
+    zIndex: 1,
+    backgroundColor: '#000',
+    width: 135,
+    height: 240,
+    borderRadius: 8,
+    elevation: 5,
+    overflow: 'hidden',
+  }
+});
 
 const MainSwitcher = () => {
   // `mode` is one of `'game'` or `'navigator'`
@@ -30,47 +39,46 @@ const MainSwitcher = () => {
   return (
     <View style={{ flex: 1, backgroundColor: 'white', position: 'relative' }}>
       <View style={{ flex: 1 }}>
-        <Page visible={mode === 'game'}>
+        <View style={ mode === 'game' ? styles.fullscreen : styles.windowed }>
           <GameScreen />
+            { mode === 'navigator' &&
+              <View style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+              }}>
+                <TouchableOpacity onPress={() => {
+                  setMode('game');
+                }}>
+                  <View style={{
+                    width: '100%',
+                    height: '100%',
+                  }}>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            }
+        </View>
+        { mode === 'game' &&
           <View style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              zIndex: 1,
-              backgroundColor: '#fff',
-              width: '100%',
-              padding: 16,
-            }}>
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            zIndex: 1,
+            width: '100%',
+            backgroundColor: '#fff',
+            padding: 16,
+          }}>
             <TouchableOpacity onPress={() => {
               setMode('navigator');
-              console.log(mode);
             }}>
               <Text style={{ fontSize: 18 }}>Return to Castle</Text>
             </TouchableOpacity>
           </View>
-        </Page>
-        <Page visible={mode === 'navigator'}>
-          <RootNavigator />
-          <View style={{
-            position: 'absolute',
-            bottom: 64,
-            left: 16,
-            zIndex: 1,
-            backgroundColor: '#eee',
-            width: 90,
-            height: 160,
-            borderRadius: 8,
-            justifyContent: 'center',
-            elevation: 5,
-          }}>
-            <TouchableOpacity onPress={() => {
-              setMode('game');
-              console.log(mode);
-            }}>
-              <Text>Return to Game</Text>
-            </TouchableOpacity>
-          </View>
-        </Page>
+        }
+        <RootNavigator />
       </View>
     </View>
   );
