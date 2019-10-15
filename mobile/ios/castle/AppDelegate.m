cd4281d5215249483c39b0d66a88f8ed42c26c40
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-10-15 15:34:33 +0300

    mobile: some more native linking code

--- mobile/ios/castle/AppDelegate.m
+++ mobile/ios/castle/AppDelegate.m
@@ -80,6 +80,14 @@ int SDL_main(int argc, char *argv[]) {
   return [RCTLinkingManager application:app openURL:url options:options];
 }
 
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+}
+
 - (void)applicationWillTerminate:(UIApplication *)application {
   [self.sdlDelegate applicationWillTerminate:application];
 }
