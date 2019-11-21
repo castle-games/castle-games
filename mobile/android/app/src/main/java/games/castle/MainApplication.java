5fcd1631fca1d947f7a76cb4a3095bfdbcf527ca
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-11-21 13:01:33 -0800

    android: fix re-adding native module

--- mobile/android/app/src/main/java/games/castle/MainApplication.java
+++ mobile/android/app/src/main/java/games/castle/MainApplication.java
@@ -7,7 +7,6 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import com.actionsheet.ActionSheetPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -31,7 +30,6 @@ public class MainApplication extends Application implements ReactApplication {
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
       packages.add(new GhostPackage());
-      packages.add(new ActionSheetPackage());
       return packages;
     }
 
