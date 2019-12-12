db44d5f6592baed9f1ec95ed63b68aef1a1fd0c5
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-12-12 11:59:06 -0800

    mobile: migrate away from 'apollo-boost'

--- mobile/js/Session.js
+++ mobile/js/Session.js
@@ -1,61 +1,101 @@
 // Maintains session state for the API server connection -- auth token and GraphQL client
 
 import AsyncStorage from '@react-native-community/async-storage';
-import ApolloClient from 'apollo-boost';
-import gql from 'graphql-tag';
+import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink, Observable } from 'apollo-link';
+import gql from 'graphql-tag';
 
 let authToken = null;
 
 export const getAuthToken = () => authToken;
 
-const typeNameToIdFieldName = {
-  Game: 'gameId',
-  User: 'userId',
-  HostedFile: 'fileId',
-  Post: 'postId',
-  ChatChannel: 'chatChannelId',
-  ChatMessage: 'chatMessageId',
-};
-
+// Based on https://www.apollographql.com/docs/react/migrating/boost-migration/
 export const apolloClient = new ApolloClient({
-  uri: 'https://api.castle.games/graphql',
-  request: operation => {
-    const headers = {};
-    headers['X-Platform'] = 'mobile';
-    if (authToken) {
-      headers['X-Auth-Token'] = authToken;
-    }
-    operation.setContext({ headers });
-  },
-  // cache: new InMemoryCache({
-  //   fragmentMatcher: new IntrospectionFragmentMatcher({
-  //     introspectionQueryResultData: {
-  //       __schema: {
-  //         types: [],
-  //       },
-  //     },
-  //   }),
-  //   dataIdFromObject: o => {
-  //     let id = o.id;
-  //     const idField = typeNameToIdFieldName[o.__typename];
-  //     if (idField) {
-  //       if (o[idField]) {
-  //         id = o[idField];
-  //       } else {
-  //         console.log(`DEBUG: Missing ID Field -- ${o.__typename} -- ${id}`);
-  //       }
-  //     }
-  //     return o.__typename + '_' + id;
-  //   },
-  //   cacheRedirects: {
-  //     Query: {
-  //       game: (_, args, { getCacheKey }) => getCacheKey({ __typename: 'Game', id: args.gameId }),
-  //       user: (_, args, { getCacheKey }) => getCacheKey({ __typename: 'User', id: args.userId }),
-  //     },
-  //   },
-  // }),
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+        );
+      }
+      if (networkError) {
+        console.log(`[Network error]: ${networkError}`);
+      }
+    }),
+    new ApolloLink(
+      (operation, forward) =>
+        new Observable(observer => {
+          let handle;
+          Promise.resolve(operation)
+            .then(operation => {
+              const headers = {};
+              headers['X-Platform'] = 'mobile';
+              if (authToken) {
+                headers['X-Auth-Token'] = authToken;
+              }
+              operation.setContext({ headers });
+            })
+            .then(() => {
+              handle = forward(operation).subscribe({
+                next: observer.next.bind(observer),
+                error: observer.error.bind(observer),
+                complete: observer.complete.bind(observer),
+              });
+            })
+            .catch(observer.error.bind(observer));
+          return () => {
+            if (handle) handle.unsubscribe();
+          };
+        })
+    ),
+    new HttpLink({
+      uri: 'https://api.castle.games/graphql',
+    }),
+  ]),
+  cache: new InMemoryCache(),
 });
+
+// Failed attempt at an Apollo cache...:
+//
+//   const typeNameToIdFieldName = {
+//     Game: 'gameId',
+//     User: 'userId',
+//     HostedFile: 'fileId',
+//     Post: 'postId',
+//     ChatChannel: 'chatChannelId',
+//     ChatMessage: 'chatMessageId',
+//   };
+//   cache: new InMemoryCache({
+//     fragmentMatcher: new IntrospectionFragmentMatcher({
+//       introspectionQueryResultData: {
+//         __schema: {
+//           types: [],
+//         },
+//       },
+//     }),
+//     dataIdFromObject: o => {
+//       let id = o.id;
+//       const idField = typeNameToIdFieldName[o.__typename];
+//       if (idField) {
+//         if (o[idField]) {
+//           id = o[idField];
+//         } else {
+//           console.log(`DEBUG: Missing ID Field -- ${o.__typename} -- ${id}`);
+//         }
+//       }
+//       return o.__typename + '_' + id;
+//     },
+//     cacheRedirects: {
+//       Query: {
+//         game: (_, args, { getCacheKey }) => getCacheKey({ __typename: 'Game', id: args.gameId }),
+//         user: (_, args, { getCacheKey }) => getCacheKey({ __typename: 'User', id: args.userId }),
+//       },
+//     },
+//   }),
+//
 
 export const initAsync = async () => {
   authToken = await AsyncStorage.getItem('AUTH_TOKEN');
