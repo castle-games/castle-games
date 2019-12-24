cff57bdda7c643229a7e86a8a8c7f61d30902454
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-12-23 16:15:30 -0800

    desktop: latest 'monaco-editor' update needs us to do this...

--- desktop/webpack.config.js
+++ desktop/webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpg|gif|wav)$/,
+        test: /\.(png|jpg|gif|wav|ttf)$/,
         use: [
           {
             loader: 'file-loader',
