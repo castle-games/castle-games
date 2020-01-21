4534b9690cdfd738f107bd95ffb2b92b9c3a486e
Ben Roth <spongebucket@gmail.com>
2020-01-21 14:29:26 -0800

    mobile: EditBlock is function component

--- mobile/js/EditBlock.js
+++ mobile/js/EditBlock.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TouchableOpacity, StyleSheet, Text, TextInput, View } from 'react-native';
-import { connectActionSheet } from '@expo/react-native-action-sheet';
+import { useActionSheet } from '@expo/react-native-action-sheet';
 
 import FastImage from 'react-native-fast-image';
 
@@ -120,15 +120,13 @@ const Dropdown = props => {
   );
 };
 
-class EditBlock extends React.Component {
-  state = {
-    // TODO: actually modify some block object somewhere
-    selectedType: 'text',
-    selectedDestination: null,
-  };
+const EditBlock = props => {
+  const [selectedType, setSelectedType] = useState('text');
+  const [selectedDestination, setSelectedDestination] = useState(null);
+  const { showActionSheetWithOptions } = useActionSheet();
 
-  _selectBlockType = () => {
-    this.props.showActionSheetWithOptions(
+  const selectBlockType = () =>
+    showActionSheetWithOptions(
       {
         title: 'Block Type',
         options: BLOCK_TYPES.map(type => type.name).concat(['Cancel']),
@@ -136,17 +134,16 @@ class EditBlock extends React.Component {
       },
       buttonIndex => {
         if (buttonIndex < BLOCK_TYPES.length) {
-          this.setState({ selectedType: BLOCK_TYPES[buttonIndex].type });
+          setSelectedType(BLOCK_TYPES[buttonIndex].type);
         }
       }
     );
-  };
 
-  _selectDestination = () => {
-    const { deck } = this.props;
+  const selectDestination = () => {
+    const { deck } = props;
     if (!deck || !deck.cards || !deck.cards.length) return false;
 
-    this.props.showActionSheetWithOptions(
+    showActionSheetWithOptions(
       {
         title: 'Destination',
         options: deck.cards.map(card => card.name).concat(['Cancel']),
@@ -154,66 +151,63 @@ class EditBlock extends React.Component {
       },
       buttonIndex => {
         if (buttonIndex < deck.cards.length) {
-          this.setState({ selectedDestination: deck.cards[buttonIndex].name });
+          setSelectedDestination(deck.cards[buttonIndex].name);
         }
       }
     );
   };
 
-  render() {
-    const { selectedType, selectedDestination } = this.state;
-    const blockType = selectedType.charAt(0).toUpperCase() + selectedType.slice(1);
-    const typeStyles = selectedType === 'choice' ? choiceTypeStyles : textTypeStyles;
-    const maybeBlockIcon =
-      selectedType === 'choice' ? (
-        <FastImage
-          style={{
-            width: 12,
-            aspectRatio: 1,
-            marginTop: 2,
-            marginLeft: 2,
-          }}
-          source={require('../assets/images/add.png')}
+  const blockType = selectedType.charAt(0).toUpperCase() + selectedType.slice(1);
+  const typeStyles = selectedType === 'choice' ? choiceTypeStyles : textTypeStyles;
+  const maybeBlockIcon =
+    selectedType === 'choice' ? (
+      <FastImage
+        style={{
+          width: 12,
+          aspectRatio: 1,
+          marginTop: 2,
+          marginLeft: 2,
+        }}
+        source={require('../assets/images/add.png')}
+      />
+    ) : null;
+  return (
+    <View style={[styles.editDescriptionContainer, typeStyles.editDescriptionContainer]}>
+      <View style={styles.editDescriptionRow}>
+        {maybeBlockIcon}
+        <TextInput
+          style={[styles.editDescriptionField, typeStyles.editDescriptionField]}
+          multiline
+          autoFocus
+          numberOfLines={2}
+          placeholder="Once upon a time..."
+          placeholderTextColor="#999"
+          onFocus={props.onTextInputFocus}
         />
-      ) : null;
-    return (
-      <View style={[styles.editDescriptionContainer, typeStyles.editDescriptionContainer]}>
-        <View style={styles.editDescriptionRow}>
-          {maybeBlockIcon}
-          <TextInput
-            style={[styles.editDescriptionField, typeStyles.editDescriptionField]}
-            multiline
-            autoFocus
-            numberOfLines={2}
-            placeholder="Once upon a time..."
-            placeholderTextColor="#999"
-            onFocus={this.props.onTextInputFocus}
+        <TouchableOpacity style={styles.dismiss} onPress={props.onDismiss}>
+          <FastImage
+            style={{
+              width: 16,
+              aspectRatio: 1,
+            }}
+            source={require('../assets/images/dismiss.png')}
           />
-          <TouchableOpacity style={styles.dismiss} onPress={this.props.onDismiss}>
-            <FastImage
-              style={{
-                width: 16,
-                aspectRatio: 1,
-              }}
-              source={require('../assets/images/dismiss.png')}
-            />
-          </TouchableOpacity>
-        </View>
-        <Text style={[styles.label, typeStyles.label]}>Block Type</Text>
-        <Dropdown onPress={this._selectBlockType} value={blockType} styleSheet={typeStyles} />
-        {selectedType == 'choice' ? (
-          <React.Fragment>
-            <Text style={[styles.label, typeStyles.label]}>Destination</Text>
-            <Dropdown
-              onPress={this._selectDestination}
-              value={selectedDestination}
-              styleSheet={typeStyles}
-            />
-          </React.Fragment>
-        ) : null}
+        </TouchableOpacity>
       </View>
-    );
-  }
-}
+      <Text style={[styles.label, typeStyles.label]}>Block Type</Text>
+      <Dropdown onPress={selectBlockType} value={blockType} styleSheet={typeStyles} />
+      {selectedType == 'choice' ? (
+        <React.Fragment>
+          <Text style={[styles.label, typeStyles.label]}>Destination</Text>
+          <Dropdown
+            onPress={selectDestination}
+            value={selectedDestination}
+            styleSheet={typeStyles}
+          />
+        </React.Fragment>
+      ) : null}
+    </View>
+  );
+};
 
-export default connectActionSheet(EditBlock);
+export default EditBlock;
